f98a2fbad7a5f89280ba6807759c97cb703abc21
usmanbukhari90 <mrbukhari900@gmail.com>
2026-09-18 16:38:14 +0500

    Part 2 is implemented with heap memory allocation

--- Analyzer.cpp
+++ Analyzer.cpp
@@ -11,9 +11,16 @@ int main(){
 
 
     int number = 5;
+    int* ivalue = new int;
+    *ivalue = 6;
 
-   cout<<"Part 1"<<endl;
+    cout<<"Part 1"<<endl;
     analyze_pointer(&number);
+
+    cout<<"Part 2"<<endl;
+    analyze_pointer(ivalue);
+
+    delete ivalue;
 
     return 0;
 }
