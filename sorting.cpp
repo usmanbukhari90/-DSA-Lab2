e7699d03eea7177607fb3cc0ea871bc7b00f9b2f
usmanbukhari90 <mrbukhari900@gmail.com>
2026-09-18 20:34:24 +0500

    some more test cases added

--- sorting.cpp
+++ sorting.cpp
@@ -34,11 +34,39 @@ void testunSortedArray(){
     cout << "testunSortedArray passed" << endl;
     
 }
+void testduplicateArray(){
+
+    int arr[5] = {1, 2, 2, 4, 5};
+    bool result = isSorted(arr,5);
+    assert(result==true);
+    cout << "testduplicateArray passed" << endl;
+    
+}
+void testsinglyArray(){
+
+    int arr[1] = {5};
+    bool result = isSorted(arr,1);
+    assert(result==true);
+    cout << "testsinglyArray passed" << endl;
+    
+}
+void testemptyArray(){
+
+    int arr[0] = {};
+    bool result = isSorted(arr,0);
+    assert(result==true);
+    cout << "testemptyArray passed" << endl;
+    
+}
+
 
 int main(){
 
    //testSortedArray();
-   testunSortedArray();
+   //testunSortedArray();
+   //testduplicateArray();
+   //testsinglyArray();
+   testemptyArray();
 
     return 0;
 }
